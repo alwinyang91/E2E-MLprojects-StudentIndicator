5857e1f5cf039bce183dd8bcb3be2c8fad99dec6
Zewen <zewenyang91@gmail.com>
2023-04-12 23:14:58 +0200

    data transformer and preprocessor pkl

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -2,12 +2,15 @@ import os
 import sys
 import pandas as pd
 
-from src.exception import CustomException
-from src.logger import logging
-
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+from src.exception import CustomException
+from src.logger import logging
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+# from src.components.model_trainer import ModelTrainerConfig
+# from src.components.model_trainer import ModelTrainer
 
 @dataclass
 class DataIngestionConfig:
@@ -37,24 +40,27 @@ class DataIngestion:
 
             test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
 
-            logging.info("Inmgestion of the data iss completed")
+            logging.info("Inmgestion of the data is completed")
 
             return(
                 self.ingestion_config.train_data_path,
                 self.ingestion_config.test_data_path
-
             )
         except Exception as e:
             raise CustomException(e,sys)
         
 
 
+# if __name__=="__main__":
+#     obj = DataIngestion()
+#     obj.initiate_data_ingestion()
+
 if __name__=="__main__":
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
-
-#     data_transformation=DataTransformation()
-#     train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+    train_data, test_data = obj.initiate_data_ingestion()
+    
+    data_transformation=DataTransformation()
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
 
 #     modeltrainer=ModelTrainer()
 #     print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
